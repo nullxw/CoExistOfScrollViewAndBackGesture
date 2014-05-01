79d224c356f50d2f72bc606be37888be5129c925
cDigger <lexingyu@lexingyudemini.lan>
2014-05-01 11:12:12 +0800

    Initial Commit

---
+++ CoExistOfScrollViewAndBackGesture/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoExistOfScrollViewAndBackGesture
+//
+//  Created by 乐星宇 on 14-5-1.
+//  Copyright (c) 2014年 Lxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CoExistOfScrollViewAndBackGesture/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoExistOfScrollViewAndBackGesture
+//
+//  Created by 乐星宇 on 14-5-1.
+//  Copyright (c) 2014年 Lxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
